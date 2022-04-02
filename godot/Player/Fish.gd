851ef21cc3f19759fa95e231ec5d38b5a3fde6ef
Alexis Schad <a@mandelbro.com>
2022-04-03 00:36:16 +0200

    Fish rotates along direction

--- godot/Player/Fish.gd
+++ godot/Player/Fish.gd
@@ -1,14 +1,20 @@
 extends KinematicBody2D
 
-const SPEED = 100
-const ACCELERATION = 200
+const SPEED = 80
+const ROTATION_SPEED = 5
+
+onready var pivot = $Pivot
 
 var target_pos = Vector2.ZERO
 var velocity = Vector2.ZERO
+onready var old_position = global_position
 
 func _physics_process(delta):
-	var direction = (target_pos - position).normalized()
-	velocity = direction * SPEED
-	velocity = move_and_slide(velocity)
+	velocity = (target_pos - position).normalized() * SPEED
+	velocity = move_and_slide(velocity, Vector2.ZERO, false, 2)
 	
-	# rotation = velocity.angle()
+func _process(delta):
+	var delta_pos = global_position - old_position
+	if delta_pos.length() > 0:
+		pivot.rotation = lerp_angle(pivot.rotation, delta_pos.angle() + PI / 2, delta * ROTATION_SPEED)
+	old_position = global_position
--- godot/Player/Player.gd
+++ godot/Player/Player.gd
@@ -13,24 +13,25 @@ onready var fishes = $Fishes
 onready var target2CirclesLeft = $Target2CirclesLeft
 onready var target2CirclesRight = $Target2CirclesRight
 
-func _physics_process(delta):
-	var children = []
-	for child in fishes.get_children():
-		children.push_back(child)
+func _physics_process(_delta):
+	var children = fishes.get_children()
 	
 	if children.size() > 0:
 		match current_pattern:
 			Pattern.CIRCLE:
 				set_target_pos_circle(children)
+				if Input.is_action_just_pressed("change_formation"):
+					current_pattern = Pattern.TWO_CIRCLES
 			Pattern.TWO_CIRCLES:
 				set_target_pos_two_circles(children)
+				if Input.is_action_just_pressed("change_formation"):
+					current_pattern = Pattern.CIRCLE
 
 func set_target_pos_circle(children):
 	for child in children:
 		child.target_pos = Vector2.ZERO
 
 func set_target_pos_two_circles(children):
-	var median_pos = Vector2.ZERO
 	var children_x = []
 	for child in children:
 		children_x.append(child.position.x)
