06fe55302d084e74db9bd2cff24ecb8c92c6aecb
Erilhir <jacob.marie.andrea@gmail.com>
2022-04-03 23:40:57 +0200

    Mise à jour de l'écran de game over -2

--- godot/Main.gd
+++ godot/Main.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
+export (int) var CAMERA_SPEED = 80
 export (int) var NB_FISH_INIT = 12
 
 var distance = 0
 var number_of_fishes = 0
 var score = 0
 var max_of_fishes = NB_FISH_INIT
+
+func _ready():
+	$AnimatedObjects/Player.connect("end_game", self, "_game_over")
+	#$AnimatedObjects/Player.connect("nb_fishes", $AnimatedObjects/GUI, "_get_nb_fishes")
 
 func _physics_process(delta):
 
@@ -17,9 +22,6 @@ func _physics_process(delta):
 	if number_of_fishes > max_of_fishes:
 		max_of_fishes = number_of_fishes
 	score = score + (distance*number_of_fishes)
-
-func _ready():
-	$AnimatedObjects/Player.connect("end_game", self, "_game_over")
 
 func _game_over():
 	get_tree().change_scene("res://Game_Over.tscn")
