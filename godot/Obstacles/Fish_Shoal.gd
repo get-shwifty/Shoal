d230580581fa347b0830db9ad7a36dc93bda88a1
Erilhir <jacob.marie.andrea@gmail.com>
2022-04-03 12:21:35 +0200

    Ajout de la mécanique de rajout de poissons - 2

--- godot/Obstacles/Fish_Shoal.gd
+++ godot/Obstacles/Fish_Shoal.gd
@@ -6,7 +6,9 @@ func _on_Area2D_body_entered(body):
 
 func add_fish(body):
 	for child in $Fishes.get_children() :
+		var gp = child.global_position
 		$Fishes.remove_child(child)
 		child.set_collision_layer(2)
 		body.get_parent().add_child(child)
+		child.global_position = gp
 	queue_free()
